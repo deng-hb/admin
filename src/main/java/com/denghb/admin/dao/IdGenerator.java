9f8713a7b3d2a19b7a1376767bdce069d6415451
denghb <i@denghb.com>
2016-08-25 21:28:36 +0800

    1

---
+++ src/main/java/com/denghb/admin/dao/IdGenerator.java
@@ -0,0 +1,16 @@
+package com.denghb.admin.dao;
+
+/**
+ * ID 生成器
+ * 
+ * @author denghb
+ *
+ */
+public class IdGenerator {
+
+	// 简单生成
+	public static long accountId(){
+		return System.currentTimeMillis();
+	}
+	
+}
